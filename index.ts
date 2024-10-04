4724c2ed60e696a9d39a97acb46e093b9c25eeac
ManueleNolli <manuele.nolli.01@gmail.com>
2024-10-04 14:52:12 +0200

    Updated handler

--- index.ts
+++ index.ts
@@ -1,10 +1,96 @@
 import 'dotenv/config'
 
-import { Handler } from 'aws-lambda';
+import {APIGatewayProxyEvent, Handler} from 'aws-lambda';
+import {sendEmail} from "./services/ses";
 
-export const handler: Handler = async (event, context) => {
-      return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
-  };
+const emailVerification = (email: string) => {
+    const emailRegex = new RegExp(/^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/)
+    return emailRegex.test(email)
+}
+
+const isSourceValid = (source: string) => {
+    return emailVerification(source)
+}
+
+const isToValid = (to: string) => {
+    return emailVerification(to)
+}
+
+const isSubjectValid = (subject: string) => {
+    return subject.length > 0
+}
+
+const isMessageValid = (message: string) => {
+    return message.length > 0
+}
+
+const validateFields = (source: string | undefined, to: string | undefined, subject: string | undefined, message: string | undefined) => {
+    if (!source || !to || !subject || !message) {
+        throw new Error('Please fill all fields')
+    }
+
+    if (!isSourceValid(source)) {
+        throw new Error('Invalid source email')
+    }
+
+    if (!isToValid(to)) {
+        throw new Error('Invalid to email')
+    }
+
+    if (!isSubjectValid(subject)) {
+        throw new Error('Invalid subject')
+    }
+
+    if (!isMessageValid(message)) {
+        throw new Error('Invalid message')
+    }
+
+    return {source, to, subject, message}
+}
+
+export const handler: Handler = async (event: APIGatewayProxyEvent, context) => {
+    const params = event.queryStringParameters
+    let source = params?.source
+    let to = params?.to
+    let subject = params?.subject
+    let message = params?.message
+
+    try {
+        ({source, to, subject, message} = validateFields(source, to, subject, message));
+    } catch (error: any) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({message: error.message}),
+        };
+    }
+
+    const emailParams = {
+        Destination: {
+            ToAddresses: [to],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: message,
+                },
+            },
+            Subject: {
+                Data: subject,
+            },
+        },
+        Source: source,
+    }
+
+    try {
+        await sendEmail(emailParams)
+        return {
+            statusCode: 200,
+            body: JSON.stringify({message: "Message Sent"}),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: "Error during sending message, please try again"}),
+        };
+    }
 };
